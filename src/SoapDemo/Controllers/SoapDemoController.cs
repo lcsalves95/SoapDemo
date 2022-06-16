b65596f934774bbddeb760609a3b9c6121dcce45
Lucas Alves <lucas.alves@ambevtech.com.br>
2022-06-16 17:11:10 -0300

    First try with SOAP integration

---
+++ src/SoapDemo/Controllers/SoapDemoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoapDemo.Controllers
+{
+    [Route("api/soap-demo")]
+    [ApiController]
+    public class SoapDemoController : ControllerBase
+    {
+        private readonly SOAPDemoSoap _demoSoap;
+
+        public SoapDemoController(SOAPDemoSoap demoSoap)
+        {
+            _demoSoap = demoSoap;
+        }
+
+        [HttpGet("add-integer")]
+        public async Task<IActionResult> AddInteger(int arg1, int arg2)
+        {
+            try
+            {
+                return Ok(await _demoSoap.AddIntegerAsync(arg1, arg2));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("divide-integer")]
+        public async Task<IActionResult> DivideInteger(int dividendo, int divisor)
+        {
+            try
+            {
+                return Ok(await _demoSoap.DivideIntegerAsync(dividendo, divisor));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/SoapDemo/Services/SoapDemoService.cs
@@ -0,0 +1,60 @@
+﻿using System.Xml;
+
+namespace SoapDemo.Services
+{
+    public class SoapDemoService : SOAPDemoSoap
+    {
+        private readonly SOAPDemoSoapClient _client;
+
+        public SoapDemoService()
+        {
+            _client = new SOAPDemoSoapClient();
+        }
+
+        public async Task<long> AddIntegerAsync(long Arg1, long Arg2)
+        {
+            return await _client.AddIntegerAsync(Arg1, Arg2);
+        }
+
+        public async Task<long> DivideIntegerAsync(long Arg1, long Arg2)
+        {
+            if (Arg2 == 0) throw new ArgumentException("Divisor não pode ser igual a zero.");
+            return await _client.DivideIntegerAsync(Arg1, Arg2);
+        }
+
+        public async Task<Person> FindPersonAsync(string id)
+        {
+            return await _client.FindPersonAsync(id);
+        }
+
+        public async Task<ArrayOfXElement> GetByNameAsync(string name)
+        {
+            return await _client.GetByNameAsync(name);
+        }
+
+        public async Task<XmlElement> GetDataSetByNameAsync(string name)
+        {
+            return await _client.GetDataSetByNameAsync(name);
+        }
+
+        public async Task<PersonIdentification[]> GetListByNameAsync(string name)
+        {
+            return await _client.GetListByNameAsync(name);
+        }
+
+        public async Task<Address> LookupCityAsync(string zip)
+        {
+            return await _client.LookupCityAsync(zip);
+        }
+
+        public async Task<string> MissionAsync()
+        {
+            return await _client.MissionAsync();
+        }
+
+        public async Task<XmlElement> QueryByNameAsync(string name)
+        {
+            return await _client.QueryByNameAsync(name);
+        }
+    }
+}
